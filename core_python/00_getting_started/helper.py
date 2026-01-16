18346d4714e0d59ae120c43cac4af9ebbbd2a3ad
securitylamb <vrishabsharma1111@gmail.com>
2026-01-16 12:52:18 +0530

    added code data types

---
+++ core_python/00_getting_started/helper.py
@@ -0,0 +1,11 @@
+def main():
+    print("name: ",__name__)
+    print("package: ",__package__)
+    print("File: ",__file__)
+
+
+
+
+if __name__ == "__main__":
+    print("hello world ......")
+    main()
---
+++ core_python/00_getting_started/script.py
@@ -0,0 +1,14 @@
+import helper
+
+def main():
+    print("name: ",__name__)
+    print("package: ",__package__)
+    print("File: ",__file__)
+    helper.main()
+
+
+
+
+if __name__ == "__main__":
+    print("hello world ......")
+    main()
